5c1dff6197326aaec60c44904656e64488ae5412
Roland Lammel <rl@quikit.at>
2020-01-19 19:14:27 +0100

    refs #31894: Add debian packaing using goreleaser

---
+++ resources/scripts/postinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+id prometheus >/dev/null 2>&1
+if [ $? -gt 0 ]; then
+  echo "Creating local user 'prometheus'"
+  useradd prometheus -c 'Prometheus exporter' --system -M -N
+fi
+echo "Adjust permissions for config files"
+chmod 640 /etc/prometheus-c5-exporter.conf
+if [ -f /etc/init.d/prometheus-c5-exporter ]; then
+  echo "Adjust permission for systemV init script"
+  chmod 755 /etc/init.d/prometheus-c5-exporter
+fi
