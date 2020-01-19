1b305187ef3ad7c7e73b555982190718ca56a80a
Roland Lammel <rl@quikit.at>
2020-01-19 20:24:13 +0100

    Cleanup for RPM packaging

--- resources/scripts/postinstall.sh
+++ resources/scripts/postinstall.sh
@@ -5,8 +5,10 @@ if [ $? -gt 0 ]; then
   echo "Creating local user 'prometheus'"
   useradd prometheus -c 'Prometheus exporter' --system -M -N
 fi
-echo "Adjust permissions for config files"
-chmod 640 /etc/prometheus-c5-exporter.conf
+if [ -f /etc/prometheus-c5-exporter.conf ]; then
+  echo "Adjust permissions for config files"
+  chmod 640 /etc/prometheus-c5-exporter.conf
+fi
 if [ -f /etc/init.d/prometheus-c5-exporter ]; then
   echo "Adjust permission for systemV init script"
   chmod 755 /etc/init.d/prometheus-c5-exporter
